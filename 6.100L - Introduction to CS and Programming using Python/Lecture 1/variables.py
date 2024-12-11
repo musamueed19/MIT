a15c46267499be29670136d39f58995e547dc8c7
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-11 23:22:08 +0500

    variables in python

---
+++ 6.100L - Introduction to CS and Programming using Python/Lecture 1/variables.py
@@ -0,0 +1,21 @@
+
+# -----------------------------
+# Some Many Objects, Now What to do with them?!
+
+# Programs usually use these object to automate those complex things, time saving us hours of time - repeatable or situtational based tasks Like --- expert systems (ES)
+
+
+# So, we at end - reach on conclusion to start assigning names to these object. So, we can refer to them later in many different places. Instead, of copy and place in my different places like i.e.,
+
+# PI = 3.1415
+
+# We now don't need to  write PI = 3.1415 in every place. We can just use PI in every place.
+# This is called variable assignment.
+
+# DIFFERENCE BETWEEN MATH VARIABLES & CS VARIABLES
+
+# Math variables - Abstract, can represent many values , can be used in many different places, can be used in many different equations, can be used. Like --- x * x = y (Where x represents all square roots)
+
+# CS variables - It is bound to one single value at a given time, can be bound to an expression (but expressions evaluate to one value)
+# Like m = 10;
+# F = m * 9.98;
