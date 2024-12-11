9c1684cd82a790cf1a29fcd0d68cdf2110f9fc30
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-11 23:28:27 +0500

    small exercise variables

--- 6.100L - Introduction to CS and Programming using Python/Lecture 1/variables.py
+++ 6.100L - Introduction to CS and Programming using Python/Lecture 1/variables.py
@@ -19,3 +19,31 @@
 # CS variables - It is bound to one single value at a given time, can be bound to an expression (but expressions evaluate to one value)
 # Like m = 10;
 # F = m * 9.98;
+
+
+# In variable assignment - if RHS has an expression - we first evaluate RHS, then  assign the value to LHS. binds a value to a name
+
+# Like - a = 10 + 20;  # 30
+
+# Variable Naming - Rules
+# 1. Must start with a letter or underscore
+# 2. Can contain letters, digits, and underscores
+# 3. Must not start with a digit
+# 4. Must not contain any special characters except underscore
+# 5. Must not be a reserved keyword (like if, else, for, while,
+# 6. Must be descriptive and meaningful
+# 7. Must be in lowercase (Python is case sensitive)
+# 8. Must not be too long (Python has a limit of 255 characters for variable names)
+# 9. Must not be too short (Python has a limit of 1 character for variable names)
+# 10. Must not be a duplicate (Python does not allow duplicate variable names)
+# -----------------------------
+
+# Exercise - Identify valid and invalid variable names
+# x = 6
+# 6 = x
+# x-y = 3+4
+# xy = 3+4
+
+xy = 3 + 4
+print(xy)  # Output: 7
+print(xy+1)  # Output: 8
