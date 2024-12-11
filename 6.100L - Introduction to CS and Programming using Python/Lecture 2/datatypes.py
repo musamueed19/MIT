66156849acc61c5b6d1274725a29a7b0c8ef968f
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-11 22:37:18 +0500

    setup, part-2

---
+++ 6.100L - Introduction to CS and Programming using Python/Lecture 2/datatypes.py
@@ -0,0 +1,14 @@
+# In python, all these are the types of objects.
+# 1) int
+# 2) float
+# 3) string
+# 4) bool, have value True or False
+# NoneType, have special value "None"
+
+print(type(5));
+
+print(type(3.0));
+
+# We are below, showing how to do Type CAST - convert into another type ##
+
+print(float(3));
