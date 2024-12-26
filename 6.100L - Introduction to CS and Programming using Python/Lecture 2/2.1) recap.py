36f1ab83b19fb3128d1bb2137bc87e60b2d7dfc3
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-26 21:50:16 +0500

    resume from pause, part-1

---
+++ 6.100L - Introduction to CS and Programming using Python/Lecture 2/2.1) recap.py
@@ -0,0 +1,20 @@
+# summary, Lecture 1
+
+# In python, we have objects. Objects in memory can be of different types.
+# Types tells python what operations you can do wtih objects.
+# Expressions evaluate to one value, and involve objects and operations
+
+# These variables basically binds a name to objects in memory. With objects you can create expressions by combining objects (Facts) with operations.
+
+# Rebinding a variables - does not means modifying the value in the memory, actually it binds the variable with the new memory location. Like
+
+radius = 2.2;
+print(radius);
+radius = radius + 1; # now variable is pointing to the new memory location.
+print(radius);
+
+
+# Coming towards new Datatype - Strings
+
+var = type(5*4);
+print(var);   # now what type of value the "var" stores. 🤔
